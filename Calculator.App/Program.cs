6a4f02ad6975e8c365ca38bcf28fc1c1429e5400
DESKTOP-R9MVMQ4\Emanuela <Grigorie.tkdcf@gmail.com>
2020-10-31 17:19:25 +0200

    Prima ora. Un calculator care aduna 4 numere cu ajutorul instructiunei IF.

---
+++ Calculator.App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculator.App
+{
+	class Program
+	{
+		static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu primul numar:");
+            string primulNumar = Console.ReadLine();
+
+            Console.WriteLine("Introdu al doilea numar:");
+            string alDoileaNumar = Console.ReadLine();
+
+            Console.WriteLine("Pentru operatia \"Adunarea\" tasteaza \"a\"");
+            Console.WriteLine("Pentru operatia \"Scaderea\" tasteaza \"b\"");
+            Console.WriteLine("Pentru operatia \"Inmultirea\" tasteaza \"c\"");
+            Console.WriteLine("Pentru operatia \"Impartirea\" tasteaza \"d\"");
+
+            string operatia = Console.ReadLine();
+
+            Console.WriteLine("se afiseazul rezultatul");
+            if (operatia == "a")
+            {
+                int resultatul = Convert.ToInt32(primulNumar) + Convert.ToInt32(alDoileaNumar);
+                Console.WriteLine(resultatul);
+            }
+            if (operatia == "b")
+            {
+                int resultatul = Convert.ToInt32(primulNumar) - Convert.ToInt32(alDoileaNumar);
+                Console.WriteLine(resultatul);
+            }
+            if (operatia == "c")
+            {
+                int resultatul = Convert.ToInt32(primulNumar) * Convert.ToInt32(alDoileaNumar);
+                Console.WriteLine(resultatul);
+            }
+            if (operatia == "d")
+            {
+                int resultatul = Convert.ToInt32(primulNumar) / Convert.ToInt32(alDoileaNumar);
+                Console.WriteLine(resultatul);
+            }
+        }
+	}
+}
