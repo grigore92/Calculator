048a3684a99331cecf08528ad34d6f71c24f6d56
Victor <victor.burca@assist.ro>
2020-12-05 11:05:15 +0200

    Am facut revert la commitul precedent in care nu era implimenatt principiul "One responsability" - adica codul nu era separat in functii diferite. git revert --no-commit e59884c2c061dc90075a381dca0c2d93085c85d5..HEAD

--- Calculator.App/Program.cs
+++ Calculator.App/Program.cs
@@ -4,104 +4,66 @@ class Program
 {
     static void Main()
     {
-        int primulTermen = Program.IntroducereaPrimuluiTermen();
-        int alDoileaTermen = Program.IntroduAlDoileaTermen();
-
-        string operatia = Program.SelectareaOperatiilor();
-        string instructiuneaAleasa = Program.SelecteazaInstructiuneaDorita();
-
-        if (instructiuneaAleasa == "x")
-        {
-            Program.CalcululCuAjutorulInstructiuniiIf(operatia, primulTermen, alDoileaTermen);
-        }
-
-        if (instructiuneaAleasa == "y")
-        {
-            Program.CalcululCuAjutorulInstructiuniiSwich(operatia, primulTermen, alDoileaTermen);  
-        }
-    }
-
-    public static int IntroducereaPrimuluiTermen()
-    {
         Console.WriteLine("Introdu primul numar");
         int primulTermen = Convert.ToInt32(Console.ReadLine());
-
-        return primulTermen;
-    }
-
-    public static int IntroduAlDoileaTermen()
-    {
         Console.WriteLine("Introdu al doilea numar");
         int alDoileaTermen = Convert.ToInt32(Console.ReadLine());
 
-        return alDoileaTermen;
-    }
-
-    public static string SelectareaOperatiilor()
-    {
         Console.WriteLine("Pentru adunare apasa tasta A");
         Console.WriteLine("Pentru scadere apasa tasta B");
         Console.WriteLine("Pentru inmultire apasa tasta C");
         Console.WriteLine("Pentru impartie apasa tasta D");
         string operatia = Console.ReadLine();
 
-        return operatia;
-    }
-
-    public static string SelecteazaInstructiuneaDorita()
-    {
         Console.WriteLine("pentru insturctiunea if apasa X");
         Console.WriteLine("pentru insturctiunea Swich apasa y");
         string instructiuneaAleasa = Console.ReadLine();
 
-        return instructiuneaAleasa;
-    }
-
-    public static void CalcululCuAjutorulInstructiuniiIf(string operatiaPrimita, int primulTermeneSelecat, int alDoileaTermenSelectat)
-
-    {
-        if (operatiaPrimita == "A")
+        if (instructiuneaAleasa == "x")
         {
-            int suma = primulTermeneSelecat + alDoileaTermenSelectat;
-            Console.WriteLine("Rezultatul operatie este " + suma);
-        }
-        if (operatiaPrimita == "B")
-        {
-            int suma = primulTermeneSelecat - alDoileaTermenSelectat;
-            Console.WriteLine("Rezultatul operatie este " + suma);
-        }
-        if (operatiaPrimita == "C")
-        {
-            int suma = primulTermeneSelecat * alDoileaTermenSelectat;
-            Console.WriteLine("Rezultatul operatie este " + suma);
-        }
-        if (operatiaPrimita == "D")
-        {
-            int suma = primulTermeneSelecat / alDoileaTermenSelectat;
-            Console.WriteLine("Rezultatul operatie este " + suma);
-        }
-    }
-
-    public static void CalcululCuAjutorulInstructiuniiSwich(string operatiaPrimita, int primulTermenSelectat, int alDoileaTermenSelectat) 
-    {
-        switch (operatiaPrimita)
-        {
-            case "A":
-                int suma = primulTermenSelectat + alDoileaTermenSelectat;
+            if (operatia == "A")
+            {
+                int suma = primulTermen + alDoileaTermen;
                 Console.WriteLine("Rezultatul operatie este " + suma);
-                break;
-            case "B":
-                int scaderea = primulTermenSelectat - alDoileaTermenSelectat;
-                Console.WriteLine("Rezultatul operatiei este " + scaderea);
-                break;
-            case "C":
-                int imultirea = primulTermenSelectat * alDoileaTermenSelectat;
-                Console.WriteLine("Rezultatul operatiei este " + imultirea);
-                break;
-            case "D":
-                int impartirea = primulTermenSelectat / alDoileaTermenSelectat;
-                Console.WriteLine("Rezultatul operatiei este " + impartirea + ". FELICITARI");
-                break;
+            }
+            if (operatia == "B")
+            {
+                int suma = primulTermen - alDoileaTermen;
+                Console.WriteLine("Rezultatul operatie este " + suma);
+            }
+            if (operatia == "C")
+            {
+                int suma = primulTermen * alDoileaTermen;
+                Console.WriteLine("Rezultatul operatie este " + suma);
+            }
+            if (operatia == "D")
+            {
+                int suma = primulTermen / alDoileaTermen;
+                Console.WriteLine("Rezultatul operatie este " + suma);
+            }
+
+        }
+        if (instructiuneaAleasa == "Y")
+        {
+            switch (operatia)
+            {
+                case "A":
+                    int suma = primulTermen + alDoileaTermen;
+                    Console.WriteLine("Rezultatul operatie este " + suma);
+                    break;
+                case "B":
+                    int scaderea = primulTermen - alDoileaTermen;
+                    Console.WriteLine("Rezultatul operatiei este " + scaderea);
+                    break;
+                case "C":
+                    int imultirea = primulTermen * alDoileaTermen;
+                    Console.WriteLine("Rezultatul operatiei este " + imultirea);
+                    break;
+                case "D":
+                    int impartirea = primulTermen / alDoileaTermen;
+                    Console.WriteLine("Rezultatul operatiei este " + impartirea + ". FELICITARI");
+                    break;
+            }
         }
     }
 }
